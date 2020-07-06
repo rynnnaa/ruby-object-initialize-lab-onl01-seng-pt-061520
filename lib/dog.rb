a4f0b0274fdbc1e24cb88643a8b478272d2a6359
Ryna Truong <rynatruong@outlook.com>
2020-07-06 15:29:31 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,17 @@
 class Dog 
+<<<<<<< HEAD
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
+=======
+  def initialize(name, breed"Mutt")
+    @name = name
+    @breed = breed
+  end
+  
+  # def breed=(breed)
+  #   @breed = "Mutt"
+  # end
+>>>>>>> 796ee274920ed4090e1b0326ba6d8fbf2da2dd88
 end
